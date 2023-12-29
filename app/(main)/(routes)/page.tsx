89090ee89581039f3381f7f8ce5a8a5e155c53e7
Yuhao <pyhao@umich.edu>
2023-12-29 03:02:46 -0500

    UPDATE: Enable Dark Mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
- 
+
 export default function Home() {
   return (
     <div className="h-screen">
-      <UserButton afterSignOutUrl="/"/>
+      <UserButton 
+        afterSignOutUrl="/" 
+      />
+      <ModeToggle />
     </div>
   )
 }
