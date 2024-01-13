103d2337b5b802bb75475fbc107fa14e06f2f5f5
Yuhao <pyhao@umich.edu>
2024-01-13 10:56:06 -0500

    FIX: Add server auto close

--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -57,6 +57,7 @@ export const CreateServerModal = () => {
       await axios.post("/api/servers", values);
       form.reset;
       router.refresh();
+      onClose();
     } catch (error) {
       console.log(error);
     }  
