e29a9dffcea547d484c17897fded70e0ae54c6a1
Yuhao <pyhao@umich.edu>
2024-01-15 10:32:37 -0500

    ADD: Server Settings

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -49,7 +49,9 @@ export const ServerHeader = ({
                     </DropdownMenuItem>
                 )}
                 {isAdmin && (
-                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem 
+                        onClick={() => onOpen("editServer", {server})}
+                        className="px-3 py-2 text-sm cursor-pointer">
                         Server Settings!
                         <Settings2 className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
