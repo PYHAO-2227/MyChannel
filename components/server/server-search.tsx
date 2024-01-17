8442570c0e09e768bcc49dd00dd1e7169d32cd8d
Yuhao <pyhao@umich.edu>
2024-01-17 11:47:45 -0500

    ADD: Search Bar

---
+++ components/server/server-search.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { Search } from "lucide-react";
+
+interface ServerSearchProps {
+    data: {
+        label: string;
+        type: "channel" | "member",
+        data: {
+          icon: React.ReactNode;
+          name: string;
+          id: string;
+        }[] | undefined
+      }[]
+}
+
+export const ServerSearch = ({
+    data
+}: ServerSearchProps) => {
+    return (
+        <>
+            <button 
+                className="group px-2 py-2 rounded-md flex items-center gap-x-2 
+                w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+            >
+                <Search className="w-4 h-4 text-zinc-500 dark:text-zinc-400" />    
+                <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
+                    Search
+                </p>
+            </button>
+        </>
+    )
+}
