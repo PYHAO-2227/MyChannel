4394b92487499da3fcd554aaf43f6a55ccf92ef0
Yuhao <pyhao@umich.edu>
2024-01-18 09:53:08 -0500

    ADD: Channel search bar

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,6 +1,9 @@
 "use client"
 
 import { Search } from "lucide-react";
+import { useState } from "react";
+import { CommandDialog, CommandEmpty, CommandInput } from "../ui/command";
+import { CommandList } from "cmdk";
 
 interface ServerSearchProps {
     data: {
@@ -17,9 +20,12 @@ interface ServerSearchProps {
 export const ServerSearch = ({
     data
 }: ServerSearchProps) => {
+    const [open, setOpen] = useState(false);
+
     return (
         <>
             <button 
+                onClick={() => setOpen(true)}
                 className="group px-2 py-2 rounded-md flex items-center gap-x-2 
                 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
             >
@@ -27,7 +33,20 @@ export const ServerSearch = ({
                 <p className="font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
                     Search
                 </p>
+                <kbd
+                    className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto"
+                >
+                    <span className="text-xs">CTRL</span>K
+                </kbd>
             </button>
+            <CommandDialog open={open} onOpenChange={setOpen}>
+                <CommandInput placeholder="Search all channels and members"/>
+                <CommandList>
+                    <CommandEmpty>
+                        No Result found
+                    </CommandEmpty>
+                </CommandList>
+            </CommandDialog>
         </>
     )
 }
