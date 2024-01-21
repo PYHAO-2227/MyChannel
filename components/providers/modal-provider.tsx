fc543853e744fdafaa7069923c02171192953ee9
Yuhao <pyhao@umich.edu>
2024-01-21 17:24:32 -0500

    ADD: Message file modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -10,6 +10,7 @@ import { LeaveServerModal } from "../modals/leave-server-modal";
 import { DeleteServerModal } from "../modals/delete-server-modal";
 import { DeleteChannelModal } from "../modals/delete-channel-modal";
 import { EditChannelModal } from "../modals/edit-channel-modal";
+import { MessageFileModal } from "../modals/message-file-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -32,7 +33,8 @@ export const ModalProvider = () => {
             <LeaveServerModal />
             <DeleteServerModal />
             <DeleteChannelModal />
-            <EditChannelModal/>
+            <EditChannelModal />
+            <MessageFileModal />
         </>
     )
 }
