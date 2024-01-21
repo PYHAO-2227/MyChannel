d32e7811f04f79cce3fda0a1b3bd440f4e77a9ef
Yuhao <pyhao@umich.edu>
2024-01-21 16:25:09 -0500

    ADD: Chat input

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import { ChannelType } from "@prisma/client";
 import { currentProfile } from "@/lib/current-profile";
 import { ChatHeader } from "@/components/chat/chat-header";
 import { db } from "@/lib/db";
+import { ChatInput } from "@/components/chat/chat-input";
 
 interface ChannelIdPageProps {
   params: {
@@ -45,6 +46,18 @@ const ChannelIdPage = async ({
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <div className="flex-1">
+        Future Messages
+      </div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
       />
     </div>
    );
